cb73e52adc3bc825a1039ba2cd1c1784d77f61ba
Katya Skripchenko <katyaskr@yahoo.com>
2022-10-25 16:39:25 -0400

    Added Event name OUT

--- install-func.sql
+++ install-func.sql
@@ -1,7 +1,7 @@
 drop function if exists unpack;
 
 create or replace function unpack(IN text, IN text, IN text[],
-    OUT name TEXT, OUT type TEXT, OUT value TEXT)
+    OUT event TEXT, OUT name TEXT, OUT type TEXT, OUT value TEXT)
     returns SETOF record 
     as :MOD,'unpack'
     LANGUAGE C IMMUTABLE;
