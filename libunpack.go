6258bfa598609c52540e98ed17b00bc46c74a889
Katya Skripchenko <katyaskr@yahoo.com>
2022-10-27 09:53:07 -0400

    More testing for crash

--- libunpack.go
+++ libunpack.go
@@ -10,8 +10,6 @@ typedef struct {
 */
 import "C"
 import (
-	"encoding/hex"
-	"fmt"
 	"log"
 	"strings"
 
@@ -81,7 +79,7 @@ func ProcessLog(inAbi *C.char, inData *C.char, inTopic0 *C.char, inTopic1 *C.cha
 	log.Printf("From go ProcessLog data: %s, Size: %d\n", data, len(data))
 	dataClean := fromHex(data)
 	log.Printf("From go ProcessLog clean data: %s, Size: %d\n", dataClean, len(dataClean))
-	dataHex, _ := hex.DecodeString(dataClean)
+	//dataHex, _ := hex.DecodeString(dataClean)
 
 	// get the matching event from abi using the hash in topics[0]
 	eventStruct, err := contractABI.EventByID(common.HexToHash(topic0))
@@ -99,91 +97,93 @@ func ProcessLog(inAbi *C.char, inData *C.char, inTopic0 *C.char, inTopic1 *C.cha
 		log.Printf("From go ProcessLog Event is anonymous: %t\n", eventStruct.Anonymous)
 	}
 
-	// how many total inputs for this Event in abi
-	numInputs := len(eventStruct.Inputs)
-	log.Printf("From go ProcessLog Number of unputs in abi for this event is: %d\n", numInputs)
+	/*
+		// how many total inputs for this Event in abi
+		numInputs := len(eventStruct.Inputs)
+		log.Printf("From go ProcessLog Number of unputs in abi for this event is: %d\n", numInputs)
 
-	// unpack the inputs from log data
-	dataInputs, err := contractABI.Unpack(eventStruct.Name, dataHex)
-	if err != nil {
-		log.Printf("From go ProcessLog Error unpacking log data\n")
-		return nil
-	}
-	dataInputsNum := len(dataInputs)
-	log.Printf("From go ProcessLog Number of Log data items is: %d\n", dataInputsNum)
-
-	// data inputs (non-indexed) + topic inputs (indexed) = total inputs?
-	if topicsNum+dataInputsNum != numInputs {
-		log.Printf("From go ProcessLog Error number of inputs does't match abi specification\n")
-		return nil
-	}
-
-	// parse topics into map
-	topicsMap := make(map[string]interface{})
-	if topicsNum > 0 {
-		// create new array of only indexed parameters
-		fields := make(abi.Arguments, 0, topicsNum)
-		for _, input := range eventStruct.Inputs {
-			if input.Indexed {
-				fields = append(fields, input)
-			}
-		}
-		err = abi.ParseTopicsIntoMap(topicsMap, fields, topicHashes)
+		// unpack the inputs from log data
+		dataInputs, err := contractABI.Unpack(eventStruct.Name, dataHex)
 		if err != nil {
-			log.Printf("From go ProcessLog Error parsing topics\n")
+			log.Printf("From go ProcessLog Error unpacking log data\n")
 			return nil
 		}
-		log.Printf("From go ProcessLog Parsed %d topics\n", len(topicsMap))
-		for key, value := range topicsMap {
-			log.Printf("From go ProcessLog Topic name: %s, value: %v\n", key, value)
-		}
-	}
+		dataInputsNum := len(dataInputs)
+		log.Printf("From go ProcessLog Number of Log data items is: %d\n", dataInputsNum)
 
-	// go slice to hold input parameters
-	params := make([](*C.InputParam), numInputs)
-
-	// loop through all the inputs
-	topicIndex := 0
-	dataIndex := 0
-	for i, input := range eventStruct.Inputs {
-		param := C.InputParam{}
-
-		param.Event = C.CString(eventStruct.Name)
-		param.Name = C.CString(input.Name)
-		param.Type = C.CString(input.Type.String())
-
-		var value interface{}
-
-		// indexed from topics, non-indexed from data
-		if input.Indexed {
-			log.Printf("From go ProcessLog Indexed INPUT NAME: %s, TYPE: %s, VALUE: %v\n", input.Name, input.Type, topicsMap[input.Name])
-
-			value = topicsMap[input.Name]
-			topicIndex++
-		} else {
-			log.Printf("From go ProcessLog NON-Indexed INPUT NAME: %s, TYPE: %s, VALUE: %v\n", input.Name, input.Type, dataInputs[dataIndex])
-			value = dataInputs[dataIndex]
-			dataIndex++
+		// data inputs (non-indexed) + topic inputs (indexed) = total inputs?
+		if topicsNum+dataInputsNum != numInputs {
+			log.Printf("From go ProcessLog Error number of inputs does't match abi specification\n")
+			return nil
 		}
 
-		// send all values as strings
-		param.Value = C.CString(fmt.Sprintf("%v", value))
-		params[i] = &param
-	}
-
-	// set num params
-	*numParams = C.int(len(params))
-
-	// convert go slice to C pointer array
-	/*
-		ret := C.malloc(C.size_t(len(params)) * C.size_t(unsafe.Sizeof(uintptr(0))))
-		pRet := (*[1<<30 - 1]*C.InputParam)(ret)
-
-		for i, item := range params {
-			pRet[i] = item
+		// parse topics into map
+		topicsMap := make(map[string]interface{})
+		if topicsNum > 0 {
+			// create new array of only indexed parameters
+			fields := make(abi.Arguments, 0, topicsNum)
+			for _, input := range eventStruct.Inputs {
+				if input.Indexed {
+					fields = append(fields, input)
+				}
+			}
+			err = abi.ParseTopicsIntoMap(topicsMap, fields, topicHashes)
+			if err != nil {
+				log.Printf("From go ProcessLog Error parsing topics\n")
+				return nil
+			}
+			log.Printf("From go ProcessLog Parsed %d topics\n", len(topicsMap))
+			for key, value := range topicsMap {
+				log.Printf("From go ProcessLog Topic name: %s, value: %v\n", key, value)
+			}
 		}
-		return (**C.InputParam)(ret)
+
+		// go slice to hold input parameters
+		params := make([](*C.InputParam), numInputs)
+
+		// loop through all the inputs
+		topicIndex := 0
+		dataIndex := 0
+		for i, input := range eventStruct.Inputs {
+			param := C.InputParam{}
+
+			param.Event = C.CString(eventStruct.Name)
+			param.Name = C.CString(input.Name)
+			param.Type = C.CString(input.Type.String())
+
+			var value interface{}
+
+			// indexed from topics, non-indexed from data
+			if input.Indexed {
+				log.Printf("From go ProcessLog Indexed INPUT NAME: %s, TYPE: %s, VALUE: %v\n", input.Name, input.Type, topicsMap[input.Name])
+
+				value = topicsMap[input.Name]
+				topicIndex++
+			} else {
+				log.Printf("From go ProcessLog NON-Indexed INPUT NAME: %s, TYPE: %s, VALUE: %v\n", input.Name, input.Type, dataInputs[dataIndex])
+				value = dataInputs[dataIndex]
+				dataIndex++
+			}
+
+			// send all values as strings
+			param.Value = C.CString(fmt.Sprintf("%v", value))
+			params[i] = &param
+		}
+
+		// set num params
+		*numParams = C.int(len(params))
+
+		// convert go slice to C pointer array
+		/*
+			ret := C.malloc(C.size_t(len(params)) * C.size_t(unsafe.Sizeof(uintptr(0))))
+			pRet := (*[1<<30 - 1]*C.InputParam)(ret)
+
+			for i, item := range params {
+				pRet[i] = item
+			}
+			return (**C.InputParam)(ret)
 	*/
+
 	return (*C.char)(C.CString(eventStruct.Name))
 }
 
