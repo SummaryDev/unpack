9be1b13640e814d6cb5d67a85bfd68169a9fe2e1
Katya Skripchenko <katyaskr@yahoo.com>
2022-10-28 20:55:52 -0400

    Move getting arguments outside of SRF_IS_FIRSTCALL

--- pg-func.c
+++ pg-func.c
@@ -16,7 +16,6 @@ Datum unpack(PG_FUNCTION_ARGS)
   const int MAX_TOPIC_SIZE = 66;
   const int MAX_NUM_TOPICS = 4;
 
-
   FuncCallContext *funcctx;
   TupleDesc tupdesc;
   AttInMetadata *attinmeta;
@@ -24,66 +23,66 @@ Datum unpack(PG_FUNCTION_ARGS)
   int max_calls;
   InputParam **params;
 
+  // get the args
+  // abi
+  char *abiArg;
+  if (!PG_ARGISNULL(0)) {
+    abiArg = text_to_cstring(PG_GETARG_TEXT_PP(0));
+    ereport(DEBUG1, (errmsg("Abi is: %s, size is: %lu", abiArg, strlen(abiArg))));
+  }
+  else {
+    abiArg = "";
+    ereport(LOG, (errmsg("Error: abi is null")));
+  }
+
+  // data
+  char *dataArg;
+  if (!PG_ARGISNULL(1)) {
+    dataArg = text_to_cstring(PG_GETARG_TEXT_PP(1));
+    ereport(DEBUG1, (errmsg("Data is: %s, size is: %lu", dataArg, strlen(dataArg))));
+  }
+  else {
+    dataArg = "";
+    ereport(LOG, (errmsg("Error: data is null")));
+  }
+
+  // topics
+  ArrayType *topics;
+  Oid eltype;
+  int16 elmlen;
+  bool elmbyval;
+  char elmalign;
+  Datum *elems;
+  bool *nulls;
+  int nelems;
+
+  topics = PG_GETARG_ARRAYTYPE_P(2);
+  eltype = ARR_ELEMTYPE(topics);
+  get_typlenbyvalalign(eltype, &elmlen, &elmbyval, &elmalign);
+  deconstruct_array(topics, eltype, elmlen, elmbyval, elmalign, &elems, &nulls, &nelems);
+  ereport(DEBUG1, (errmsg("Number of topics is: %d", nelems)));
+
+  // maximum number of topics is 4, size is 66
+  char topicsArg[MAX_NUM_TOPICS][MAX_TOPIC_SIZE + 1];
+
+  // copy to array of strings to pass to go lib
+  for (int i = 0; i < nelems; i++) {
+    strncpy (topicsArg[i], text_to_cstring(DatumGetTextP(elems[i])), MAX_TOPIC_SIZE);
+    topicsArg[i][MAX_TOPIC_SIZE] = '\0';
+    ereport(DEBUG1, (errmsg("Topic[%d] is: %s, size is: %lu", i, topicsArg[i], strlen(topicsArg[i]))));
+  }
+
+  // memset the rest of the topics that are empty
+  for (int i = nelems; i < MAX_NUM_TOPICS; i++) {
+    memset(topicsArg[i], '\0', MAX_TOPIC_SIZE + 1);
+  }
+
   // stuff done only on the first call of the function
   if (SRF_IS_FIRSTCALL()) {
 
     MemoryContext oldcontext;
     funcctx = SRF_FIRSTCALL_INIT();
     oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
-
-    // get the args
-    // abi
-    char *abiArg;
-    if (!PG_ARGISNULL(0)) {
-      abiArg = text_to_cstring(PG_GETARG_TEXT_PP(0));
-      ereport(DEBUG1, (errmsg("Abi is: %s, size is: %lu", abiArg, strlen(abiArg))));
-    }
-    else {
-      abiArg = "";
-      ereport(LOG, (errmsg("Error: abi is null")));
-    }
-
-    // data
-    char *dataArg;
-    if (!PG_ARGISNULL(1)) {
-      dataArg = text_to_cstring(PG_GETARG_TEXT_PP(1));
-      ereport(DEBUG1, (errmsg("Data is: %s, size is: %lu", dataArg, strlen(dataArg))));
-    }
-    else {
-      dataArg = "";
-      ereport(LOG, (errmsg("Error: data is null")));
-    }
-
-    // topics
-    ArrayType *topics;
-    Oid eltype;
-    int16 elmlen;
-    bool elmbyval;
-    char elmalign;
-    Datum *elems;
-    bool *nulls;
-    int nelems;
-
-    topics = PG_GETARG_ARRAYTYPE_P(2);
-    eltype = ARR_ELEMTYPE(topics);
-    get_typlenbyvalalign(eltype, &elmlen, &elmbyval, &elmalign);
-    deconstruct_array(topics, eltype, elmlen, elmbyval, elmalign, &elems, &nulls, &nelems);
-    ereport(DEBUG1, (errmsg("Number of topics is: %d", nelems)));
-
-    // maximum number of topics is 4, size is 66
-    char topicsArg[MAX_NUM_TOPICS][MAX_TOPIC_SIZE + 1];
-
-    // copy to array of strings to pass to go lib
-    for (int i = 0; i < nelems; i++) {
-      strncpy (topicsArg[i], text_to_cstring(DatumGetTextP(elems[i])), MAX_TOPIC_SIZE);
-      topicsArg[i][MAX_TOPIC_SIZE] = '\0';
-      ereport(DEBUG1, (errmsg("Topic[%d] is: %s, size is: %lu", i, topicsArg[i], strlen(topicsArg[i]))));
-    }
-
-    // memset the rest of the topics that are empty
-    for (int i = nelems; i < MAX_NUM_TOPICS; i++) {
-      memset(topicsArg[i], '\0', MAX_TOPIC_SIZE + 1);
-    }
 
     // for now passing all four topics separately
     int numParams;
