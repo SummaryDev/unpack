9ffb81c37f9832cd94cb5eb8f89d84628fbd23d1
Katya Skripchenko <katyaskr@yahoo.com>
2022-10-29 20:43:31 -0400

    Changed to Materialize method

--- pg-func.c
+++ pg-func.c
@@ -4,6 +4,7 @@
 #include "utils/array.h"
 #include "utils/lsyscache.h"
 #include "utils/builtins.h"
+#include "miscadmin.h"
 #include "libunpack.h"
 #include <stdio.h>
 
@@ -16,11 +17,9 @@ Datum unpack(PG_FUNCTION_ARGS)
   const int MAX_TOPIC_SIZE = 66;
   const int MAX_NUM_TOPICS = 4;
 
-  FuncCallContext *funcctx;
+  ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+  Tuplestorestate *tupstore;
   TupleDesc tupdesc;
-  AttInMetadata *attinmeta;
-  int call_cntr;
-  int max_calls;
   InputParam **params;
 
   // get the args
@@ -77,80 +76,64 @@ Datum unpack(PG_FUNCTION_ARGS)
     memset(topicsArg[i], '\0', MAX_TOPIC_SIZE + 1);
   }
 
-  // stuff done only on the first call of the function
-  if (SRF_IS_FIRSTCALL()) {
+  /* check to see if caller supports us returning a tuplestore */
+  if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
+      ereport(ERROR,
+              (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                errmsg("set-valued function called in context that cannot accept a set")));
+                
+  if (!(rsinfo->allowedModes & SFRM_Materialize) ||
+          rsinfo->expectedDesc == NULL)
+      ereport(ERROR,
+              (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                errmsg("materialize mode required, but it is not allowed in this context")));
 
-    MemoryContext oldcontext;
-    funcctx = SRF_FIRSTCALL_INIT();
-    oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+  if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+        ereport(ERROR,
+                (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                  errmsg("function returning record called in context "
+                        "that cannot accept type record")));
 
-    // for now passing all four topics separately
-    int numParams;
-    char *errMsg = "";
-    InputParam **params = ProcessLog(abiArg, dataArg, topicsArg[0], topicsArg[1], topicsArg[2], topicsArg[3], &numParams, &errMsg);
+  MemoryContext per_query_ctx = rsinfo->econtext->ecxt_per_query_memory;
+  MemoryContext oldcontext = MemoryContextSwitchTo(per_query_ctx); 
+  tupstore = tuplestore_begin_heap(false, false, work_mem);
+  rsinfo->returnMode = SFRM_Materialize;
+  rsinfo->setResult = tupstore;
+  rsinfo->setDesc = tupdesc;
+  MemoryContextSwitchTo(oldcontext);
 
-    // no result returned from ProcessLog - report it and continue
-    if ((numParams == 0) || (!params)) {
-      ereport(LOG, (errmsg("ProcessLog returned NO results, %s", errMsg)));
-    }
-    else {
-      ereport(LOG, (errmsg("ProcessLog returned %d results", numParams))); 
-    }
+  // for now passing all four topics separately
+  int numParams;
+  char *errMsg = "";
+  params = ProcessLog(abiArg, dataArg, topicsArg[0], topicsArg[1], topicsArg[2], topicsArg[3], &numParams, &errMsg);
 
-    for (int i = 0; i < numParams; i++) {
-      InputParam *param = *(params + i);
-      ereport(DEBUG1, (errmsg("ProcessLog returned - Name: %s, Type: %s, Value: %s, index: %d", param->Name, param->Type, 
-          param->Value, i)));
-    }
-  
-    funcctx->max_calls = numParams;
-    funcctx->user_fctx = params;
-
-    if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
-          ereport(ERROR,
-                  (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-                    errmsg("function returning record called in context "
-                          "that cannot accept type record")));
-
-    attinmeta = TupleDescGetAttInMetadata(tupdesc);
-    funcctx->attinmeta = attinmeta;
-    MemoryContextSwitchTo(oldcontext);
-  }
-    
-  funcctx = SRF_PERCALL_SETUP();
-  call_cntr = funcctx->call_cntr;
-  max_calls = funcctx->max_calls;
-  attinmeta = funcctx->attinmeta; 
-  params = funcctx->user_fctx;
-
-  if (call_cntr < max_calls) {
-    char **values;
-    HeapTuple tuple;
-    Datum result;
-    InputParam *param = *(params + call_cntr);
-
-    ereport(DEBUG1, (errmsg("IN LOOP, call_cntr: %d, max_call: %d, param name: %s, param size: %lu",
-        call_cntr,  max_calls, param->Name, strlen(param->Name))));
-  
-    values = (char **) palloc(4 * sizeof(char *));
-    values[0] = (char *) palloc((strlen(param->Event)+1) * sizeof(char));
-    values[1] = (char *) palloc((strlen(param->Name)+1) * sizeof(char));
-    values[2] = (char *) palloc((strlen(param->Type)+1) * sizeof(char));
-    values[3] = (char *) palloc((strlen(param->Value)+1) * sizeof(char));
-
-    strcpy(values[0], param->Event);
-    strcpy(values[1], param->Name);
-    strcpy(values[2], param->Type);
-    strcpy(values[3], param->Value);
-  
-    tuple = BuildTupleFromCStrings(attinmeta, values);
-    result = HeapTupleGetDatum(tuple);
-    ereport(DEBUG1, (errmsg("IN LOOP 1 call_cntr: %d, max_call: %d",call_cntr,  max_calls)));
-
-    SRF_RETURN_NEXT(funcctx, result);
+  // no result returned from ProcessLog - report it and continue
+  if ((numParams == 0) || (!params)) {
+    ereport(LOG, (errmsg("ProcessLog returned NO results, %s", errMsg)));
   }
   else {
-    ereport(DEBUG1, (errmsg("Calling SRF_RETURN_DONE, call_cntr: %d, max_call: %d",call_cntr,  max_calls)));
-    SRF_RETURN_DONE(funcctx);
+    ereport(LOG, (errmsg("ProcessLog returned %d results", numParams))); 
   }
+
+  for (int i = 0; i < numParams; i++) {
+    Datum values[4];
+    bool nulls[4];
+    InputParam *param = *(params + i);  
+       
+    ereport(LOG, (errmsg("ProcessLog returned - Event: %s, Name: %s, Type: %s, Value: %s", 
+                  param->Event, param->Name, param->Type, param->Value)));
+
+    values[0] = CStringGetTextDatum(param->Event);
+    values[1] = CStringGetTextDatum(param->Name);
+    values[2] = CStringGetTextDatum(param->Type);
+    values[3] = CStringGetTextDatum(param->Value);
+    nulls[0] = false;
+    nulls[1] = false;
+    nulls[2] = false;
+    nulls[3] = false;
+
+    tuplestore_putvalues(tupstore, tupdesc, values, nulls);
+  }
+
+  return (Datum) 0;
 }
