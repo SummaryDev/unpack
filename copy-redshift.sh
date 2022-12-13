ab82ba2ba6097dc539a5b7ad256b87699ed17640
Oleg Abdrashitov on pg <olegabu73@gmail.com>
2022-12-13 07:58:22 +0000

    from pg

--- copy-redshift.sh
+++ copy-redshift.sh
@@ -9,8 +9,9 @@ while [ "$d" != $until ]; do
 
   aws s3 sync s3://aws-public-blockchain/v1.0/eth/logs/date=$d/ s3://aws-public-blockchain-copy/logs/date=$d/
 
-  aws redshift-data execute-statement --cluster-identifier redshift-cluster-1 --database dev --db-user awsuser \
-  --sql "COPY dev.eth.logs FROM 's3://aws-public-blockchain-copy/logs/date=$d/' IAM_ROLE 'arn:aws:iam::729713441316:role/service-role/AmazonRedshift-CommandsAccessRole-20221031T131305' FORMAT AS PARQUET SERIALIZETOJSON"
+  #  aws redshift-data execute-statement --cluster-identifier redshift-cluster-1 --database dev --db-user awsuser --sql \
+      psql -c \
+  "COPY dev.eth.logs FROM 's3://aws-public-blockchain-copy/logs/date=$d/' IAM_ROLE 'arn:aws:iam::729713441316:role/service-role/AmazonRedshift-CommandsAccessRole-20221031T131305' FORMAT AS PARQUET SERIALIZETOJSON"
 
   d=$(date -I -d "$d + 1 day")
 
